ee16787b2d98e4531fd9d888ffef15e4e3571cc6
JesusL <thedarkness2555@gmail.com>
2021-06-25 14:27:50 -0500

    Se realiza la calculadora utilizando funcines lambda

--- src/main/java/calculadoralambda/CalculadoraLambda.java
+++ src/main/java/calculadoralambda/CalculadoraLambda.java
@@ -1,2 +1,30 @@
 package calculadoralambda;
 
+import java.util.function.BiFunction;
+import java.util.stream.IntStream;
+
+public class CalculadoraLambda {
+
+    public static void main(String[] args) {
+        BiFunction<Integer, Integer, Integer> suma = (a, b) ->  a+b;
+        BiFunction<Integer, Integer, Integer> resta = (a,b) ->  a-b;
+
+        BiFunction<Integer, Integer, Integer> multiplicacion = (a,b) -> IntStream.range(0,b+1)
+                .reduce((acumulador, numero)-> {
+
+                    return suma.apply(acumulador, a);
+                }).getAsInt();
+
+
+        BiFunction<Integer, Integer, Integer> divi = (a,b) -> IntStream.range(0,a)
+                .reduce((acumulador, numero)->
+                        multiplicacion.apply(numero,b)<= a?suma.apply(acumulador,1):acumulador).getAsInt();
+
+        System.out.println(suma.apply(10,12));
+        System.out.println(resta.apply(8,5));
+        System.out.println(multiplicacion.apply(6,5));
+        System.out.println(divi.apply(27,3));
+
+    }
+
+}
